f6d1d2882f163941b06c9cdd0b5c4c3887951384
phanmemquanli <tulieuhopthanh@gmail.com>
2026-05-16 21:32:37 +0700

    Add files via upload

---
+++ ketnoi.js
@@ -0,0 +1,19 @@
+/* ==========================================================================
+   TỆP CẤU HÌNH HỆ THỐNG - FRONTEND (ketnoi.js)
+   (Chứa toàn bộ thông tin định danh, liên kết của giao diện người dùng)
+   ========================================================================== */
+var ddungApi = "https://script.google.com/macros/s/AKfycbwx-SfK2k6Sgf2oLQCu1Jt55clwz0nuke8YUVv8QgttrmqxgcDti7c8WA8x8jzh5CHy/exec"; 
+
+// --- CÁC BIẾN CẤU HÌNH ĐỊNH DANH HỆ THỐNG ---
+var PGV_LINK_LOGO = "https://i.ibb.co/d0JNW94Z/logo-l-a-ch-n6.png"; 
+var PGV_TEN_TRUONG = "Trường TH&THCS Hợp Thành";
+var PGV_TEN_TRUONG_UP = "TRƯỜNG TH&THCS HỢP THÀNH";
+var PGV_TEN_PM = "Quản lý Nhiệm vụ";
+var PGV_TEN_PM_UP = "QUẢN LÝ CÔNG VIỆC";
+var PGV_PHIEN_BAN = "V2.3.2026";
+var PGV_TAC_GIA = "Thiết kế và phát triển\nHoàng Ngọc Lâm"; // Cập nhật đúng định dạng 2 dòng
+var PGV_GOOGLE_CLIENT_ID = "407480994586-m6fpq6sfcc90qqj9k08rsmi1lge6br94.apps.googleusercontent.com";
+var PGV_MO_TA = "Hệ thống quản lý hiệu quả công việc nội bộ - " + PGV_TEN_TRUONG;
+
+// --- CÁC LIÊN KẾT MỞ RỘNG (KHÔNG ĐỂ LINK CỨNG TRONG HTML) ---
+var PGV_LINK_DGC_NAM = "https://quanlylaocai.github.io/danhgiacuoinam/index.html";
