90c3bbbf0cd05300d64cdcae9b739c2dcf1bdcee
phanmemquanli <tulieuhopthanh@gmail.com>
2026-06-09 08:52:21 +0700

    Update ketnoi.js

--- ketnoi.js
+++ ketnoi.js
@@ -12,7 +12,7 @@ var PGV_TEN_PM = "Quản lý Nhiệm vụ";
 var PGV_TEN_PM_UP = "QUẢN LÝ CÔNG VIỆC";
 var PGV_PHIEN_BAN = "V2.3.2026";
 var PGV_TAC_GIA = "Thiết kế và phát triển\nHoàng Ngọc Lâm"; // Cập nhật đúng định dạng 2 dòng
-var PGV_GOOGLE_CLIENT_ID = "407480994586-m6fpq6sfcc90qqj9k08rsmi1lge6br94.apps.googleusercontent.com";
+var PGV_GOOGLE_CLIENT_ID = "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com";
 var PGV_MO_TA = "Hệ thống quản lý hiệu quả công việc nội bộ - " + PGV_TEN_TRUONG;
 
 // --- CÁC LIÊN KẾT MỞ RỘNG (KHÔNG ĐỂ LINK CỨNG TRONG HTML) ---
